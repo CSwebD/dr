c19a95a60c3bd94dcc92cb80808900c6eadb76d6
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2024-07-02 20:05:17 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('../pages/footer.html')
+    fetch('../footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
