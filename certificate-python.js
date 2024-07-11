26e498a7734ff84815021527bfcb9019464bf436
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2024-07-11 01:09:23 +0100

    Add files via upload

--- certificate-python.js
+++ certificate-python.js
@@ -24,51 +24,31 @@ async function generateCertificate() {
         // Add certificate title
         doc.setFontSize(30);
         doc.setFont('helvetica', 'bold');
-        doc.text('Certificate of Completion', 148.5, 60, null, null, 'center');
+        doc.text('Certificate of Completion', 148.5, 80, null, null, 'center');
 
         // Add body text
         doc.setFontSize(12);
         doc.setFont('helvetica', 'normal');
-        doc.text(`This certificate of completion confirms that`, 148.5, 80, null, null, 'center');
+        doc.text(`This certificate of completion confirms that`, 148.5, 100, null, null, 'center');
 
         // Add recipient name
         doc.setFontSize(24);
         doc.setFont('helvetica', 'bold');
-        doc.text(name, 148.5, 90, null, null, 'center');
+        doc.text(name, 148.5, 110, null, null, 'center');
 
         // Add body text
         doc.setFontSize(12);
         doc.setFont('helvetica', 'normal');
-        doc.text(`has successfully completed`, 148.5, 102, null, null, 'center');
+        doc.text(`has successfully completed`, 148.5, 122, null, null, 'center');
 
         // Add course completion text
         doc.setFontSize(20);
         doc.setFont('helvetica', 'bold');
-        doc.text(`Python - Full Course for Beginners`, 148.5, 110, null, null, 'center');
-
-        // Add course specifics
-        doc.setFontSize(18);
-        doc.setFont('helvetica', 'normal');
-        doc.text(`You achieved and built projects such as:`, 148.5, 130, null, null, 'center');
-
-        // Project 1
-        doc.setFontSize(14);
-        doc.setFont('helvetica', 'normal');
-        doc.text(`- Image Classification with Neural Networks;`, 148.5, 138, null, null, 'center');
-
-        // Project 2
-        doc.setFontSize(14);
-        doc.text(`- Sentiment Text Analysis;`, 148.5, 146, null, null, 'center');
-
-        // Project 3
-        doc.setFontSize(14);
-        doc.text(`- Self-Driving AI Car Simulation.`, 148.5, 154, null, null, 'center');
+        doc.text(`Python - Full Course for Beginners`, 148.5, 130, null, null, 'center');
 
         // Add date
         doc.setFontSize(12);
-        doc.text(`Date: ${dateString}`, 148.5, 175, null, null, 'center');
-
-
+        doc.text(`Date: ${dateString}`, 148.5, 150, null, null, 'center');
 
         // Save PDF
         doc.save('Certificate_of_Completion.pdf');
